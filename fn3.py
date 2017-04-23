0b277caacf9c68a3796413e388864b308bc27ff4
denfer57 <jordan.fromeyer@gmail.com>
2017-04-23 23:24:35 +0200

    Debugg fonction 3

--- fn3.py
+++ fn3.py
@@ -1,10 +1,10 @@
 #retourne qui est le sujet du verbe dans la phrase donnée.
-from spacy.en import English
-nlp = English()
+from nltk.tag import pos_tag
+
 def getSubject(sentence):
-        toks_with_subject=[]
-        doc = nlp(sentence)
-        toks_with_subject = [tok for tok in doc if (tok.dep_ == "nsubj") ]
+        toks_with_subject = []
+        doc = pos_tag(sentence.split())
+        toks_with_subject = [word for word,pos in doc if pos == 'PRP']
         return toks_with_subject
 
-print(getSubject('I shot an elephant'))
+print(getSubject('I shot an elephant in Miami'))
