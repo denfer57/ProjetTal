2b23c1f34e0637d6ee81b989af567620a06844f8
denfer57 <jordan.fromeyer@gmail.com>
2017-04-23 19:16:09 +0200

    Main
    Main

--- Gaspard/src/main.py
+++ Gaspard/src/main.py
@@ -1,1 +1,38 @@
-/home/pols12/Documents/Scolaire/Sciences Cognitives/L3/S6/Ingénierie linguistique - Amblard/Travail de groupe – Qui a tué qui/main.py
+#On importe le corpus
+#from nltk.corpus import PlaintextCorpusReader
+#wordlists = PlaintextCorpusReader('../../', 'Corpus.txt')
+
+#corpus = wordlists.words('Corpus.txt')
+
+#Rechercher dans text1 :
+#res = corpus.concordance("murderer")
+#import nltk, re, pprint
+from nltk import word_tokenize
+def getTokens():
+	f=open('../../Corpus.txt')
+	raw=f.read()
+	tokens = word_tokenize(raw)
+	return tokens
+	
+
+#Recherche des synonymes
+from nltk.corpus import wordnet as wn
+def getSynonyms(word):
+    list=[]
+	for e in wn.synsets(word):
+		list+=e.lemma_names()
+	return list
+
+print(getSynonyms('murderer'))
+#print(type(corpus)))
+
+#retourne qui est le sujet du verbe dans la phrase donnée.
+from spacy.en import English
+nlp = English()
+def getSubject(sentence):
+        subject=[]
+        doc = nlp(sentence)
+        subject = [tok for tok in doc if (tok.dep_ == "nsubj") ]
+        return subject
+
+print(getSubject('I shot an elephant'))
