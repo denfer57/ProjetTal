c7622e5ae397bb27f1c159c15e1ccc896d3cea4c
Paul Sarrassat <poltron54@gmail.com>
2017-04-24 01:39:42 +0200

    fn1: retourner plutôt qu'afficher

--- fn1.py
+++ fn1.py
@@ -4,8 +4,4 @@ def synKill():
     tab=[]
     for ss in wn.synset('kill.v.01').hyponyms():
         tab += ss.lemma_names()
-    print(tab)
-
-def listeCorpus(listeSynsets): #prend une liste de synsets et retourne la liste des phrases du corpus ayant ces synsets
-    
-    return listePhrases
+    return tab
