39e32893023b1fb4738a0513f845a9592b844062
denfer57 <jordan.fromeyer@gmail.com>
2017-04-23 23:09:34 +0200

    Debugg function5
    Fonction5 marche

--- fn5.py
+++ fn5.py
@@ -1,11 +1,11 @@
 #retourne le lieu associé au verbe dans la phrase donnée.
-from spacy.en import English
-nlp = English()
+from nltk.tag import pos_tag
+
 def getPlace(sentence):
-        toks_with_subject=[]
-        doc = nlp(sentence)
-        toks_with_subject = [tok for tok in doc if (tok.dep_ == "place") ]
-        return toks_with_subject
+        toks_with_place=[]
+        doc = pos_tag(sentence.split())
+        toks_with_place = [word for word,pos in doc if pos == 'NNP']
+        return toks_with_place
 
 print(getPlace('I shot an elephant in Miami'))
 
